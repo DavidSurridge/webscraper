199910bb66add28ffe63292c04ce2060659da5b2
David Surridge <davidsurridge@gmail.com>
2018-06-17 19:31:12 +0100

    small adjustment to use interface in DoScrape implementation

--- src/scraper/DoScrape.java
+++ src/scraper/DoScrape.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class DoScrape {
 
   public static void main(String[] args) {
-    WiproWebCrawler crawler = new WiproWebCrawler();
+    WebCrawlerInterface crawler = new WiproWebCrawler();
     Set<String> sitemap = crawler.startWebScrape("https://wiprodigital.com/");
     crawler.convertToXml(sitemap);
   }
